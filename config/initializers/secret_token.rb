dc602aea51483bb328740f36987fed0e9156b0b2
brownmagik352 <asuman@dhcp-128-36-173-34.wireless.yale.edu>
2012-12-14 16:22:08 -0500

    initial commit with learning first_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '32c6dc31a86ab00dddcde537b8fcc00d78fc3331b8b01069331af60fb591ca3d935324ac6f8886e74e0831619fcc9684cfe4953a4d83f8ae3ff6ae364b8c3695'
